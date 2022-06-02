50c96690f6be3752d7c6d92d7815a3928eeb413a
bydavid1 <byronjimenez9911@gmail.com>
2022-06-02 07:43:40 -0600

    :card_file_box: Role and User associations

---
+++ models/role.js
@@ -0,0 +1,33 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Role extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Role.hasMany(models.User, {
+        foreignKey: 'role_id'
+      })
+    }
+  }
+  Role.init({
+    role_name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true
+    },
+    role_description: {
+      type: Sequelize.STRING,
+      allowNull: true
+    },
+  }, {
+    sequelize,
+    modelName: 'Role',
+  });
+  return Role;
+};
