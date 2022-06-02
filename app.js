de6004ddcc51cee00f139c3869a3963de1ba011a
Byron Jimenez <bmartinez@tuappamigo.com>
2022-06-01 20:57:07 -0600

    :tada: Setup project

---
+++ app.js
@@ -0,0 +1,58 @@
+var http = require('http'),
+    path = require('path'),
+    express = require('express'),
+    bodyParser = require('body-parser'),
+    session = require('express-session'),
+    cors = require('cors'),
+    passport = require('passport'),
+    errorhandler = require('errorhandler'),
+    morgan = require('morgan')
+
+/// get environment
+let environment = process.env.NODE_ENV;
+
+// setup express
+var app = express();
+
+// config app cors
+app.use(cors());
+
+// Normal express config defaults
+app.use(morgan);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// config express session package
+app.use(session({ secret: 'conduit', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false  }));
+
+if (environment !== 'production') {
+  // development error handler
+  app.use(errorhandler());
+
+  app.use(function(err, req, res, next) {
+    console.log(err.stack);
+
+    res.status(err.status || 500);
+
+    res.json({'errors': {
+      message: err.message,
+      error: err
+    }});
+  });
+} 
+
+// import all routes
+
+// production error handler
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({'errors': {
+    message: err.message,
+    error: {}
+  }});
+});
+
+// start server
+var server = app.listen( process.env.PORT || 3000, function(){
+  console.log('Listening on port ' + server.address().port);
+});
