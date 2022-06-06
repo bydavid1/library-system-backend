8ca9c5c5485d8cf07d9d82534ce1f3df4407964a
bydavid1 <byronjimenez9911@gmail.com>
2022-06-05 22:53:13 -0600

    :truck: Docker environment configured

--- models/index.js
+++ models/index.js
@@ -2,18 +2,23 @@
 
 const fs = require('fs');
 const path = require('path');
+const dbConfig = require("../config/database");
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+  host: dbConfig.HOST,
+  dialect: dbConfig.dialect,
+  port: dbConfig.port,
+  operatorsAliases: false,
+  pool: {
+    max: dbConfig.pool.max,
+    min: dbConfig.pool.min,
+    acquire: dbConfig.pool.acquire,
+    idle: dbConfig.pool.idle
+  }
+});
 
 fs
   .readdirSync(__dirname)
